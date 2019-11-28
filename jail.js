7a78728ad089c74a91e12edb5f52391cd082fd6c
Rayne <56103163+garbagepunks@users.noreply.github.com>
2019-11-27 21:20:52 -0600

    Update jail.js

--- jail.js
+++ jail.js
@@ -1,44 +1,58 @@
-const Discord = require("discord.js");
-const ms = require("ms");
+const Discord = require('discord.js');
+const fs = require("fs");
+const { stripIndents } = require("common-tags");
+
+const warnings = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports = {
-	name: 'jail',
-	description: 'Removes all roles from a user except the restricted prisoner role',
+	name: 'warn',
+	description: 'Issues a warning to a user.',
+	usage: 'warn [username] [reason]',
 	execute(message, args) {
 
-	async () => {
+try {
+	if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You do not have permission to warn someone.");
+	let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	if(!user) return message.reply("Can't find user.");
 
-	let jUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-	if(!jUser) return message.channel.send("Couldn't find user.");
-	let jreason = args.join(" ").slice(22);
-	if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You do not have permission to jail someone.")
-	let jrole = message.guild.roles.find(role => role.name === "Prisoner");
-	if(jUser.hasPermission("BAN_MEMBERS")) return message.channel.send("That user can't be put in jail by you.");
+	let wrole = message.guild.roles.find(role => role.name === "WARNED");
+	//let jrole = message.guild.roles.find('name', 'Prisoner');
+	let reason = args.join(" ").slice(22);
+	if(!reason) return message.reply("Please provide a reason for warning this user.");
+//	let warnlevel = warnings[user.id].warnings;
 
-	let jailEmbed = new Discord.RichEmbed()
-	.setDescription("Jailed for 24 Hours")
-	.setColor("#8D021F")
-	.addField("Jailed User", `${jUser}`)
-	.addField("Reason", jreason)
-	.addField("Jailed By", `${message.author}`)
-	.setTimestamp();
+	if(!warnings[user.id]) warnings[user.id] = {
+		warnings: 0
+		};
 
-	let jailchannel = message.guild.channels.get("632769031199522826");
-	if(!jailchannel) return message.channel.send("Couldn't find logs channel.");
+	warnings[user.id].warnings++;
 
-		try {
-			await jUser.removeRoles(jUser.roles)
-			await jUser.addRole(jrole.id)
-			await jailchannel.send(jailEmbed)
-			await message.delete();
-		} catch(e) {
-			console.error(e);
-		}
-	}
+	fs.writeFile("./warnings.json", JSON.stringify(warnings, null, 4), (err) => {
+		if (err) console.log(err);
+	});
 
-	/* setTimeout(function() {
-		jUser.removerole.jrole.id;
-	}, ms("24h")); */
+	let warnEmbed = new Discord.RichEmbed()
+	.setTitle("Warning Issued")
+	.setColor("#ffff00")
+	.setTimestamp()
+	.setDescription(stripIndents`**Warned User:** ${user}
+	**Reason:** ${reason}
+	**Warnings:** ${warnings[user.id].warnings}
+	**Warned By:** ${message.author.username}`);
 
+
+	message.delete();
+	let warnchannel = message.guild.channels.find(channel => channel.name === "logs");
+	if(!warnchannel) return message.channel.send("Couldn't find logs channel.");
+
+	warnchannel.send(warnEmbed);
+
+//	if(this.client.warnings.get(`${user.id}`, "warnings") == 1) {
+//		user.addrole(wrole.id);
+//	}
+
+}catch (error) {
+	console.log(error);
 }
-};
+}
+}

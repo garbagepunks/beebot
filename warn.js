703e7fd248c3b2bc36f0dedda5a15790294e20bb
Rayne <56103163+garbagepunks@users.noreply.github.com>
2019-11-27 21:46:18 -0600

    Update warn.js

--- warn.js
+++ warn.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const fs = require("fs");
+const { stripIndents } = require("common-tags");
 
 const warnings = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
@@ -14,10 +15,8 @@ try {
 	let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 	if(!user) return message.reply("Can't find user.");
 
-	let wrole = message.guild.roles.find('name', 'WARNED');
-	//let jrole = message.guild.roles.find('name', 'Prisoner');
 	let reason = args.join(" ").slice(22);
-//	let warnlevel = warnings[user.id].warnings;
+	if(!reason) return message.reply("Please provide a reason for warning this user.");
 
 	if(!warnings[user.id]) warnings[user.id] = {
 		warnings: 0
@@ -25,25 +24,18 @@ try {
 
 	warnings[user.id].warnings++;
 
-	fs.writeFile("./warnings.json", JSON.stringify(warnings), (err) => {
+	fs.writeFile("./warnings.json", JSON.stringify(warnings, null, 4), (err) => {
 		if (err) console.log(err);
 	});
 
 	let warnEmbed = new Discord.RichEmbed()
 	.setTitle("Warning Issued")
 	.setColor("#ffff00")
-	.addField("Warned User:", `${user}`)
-	.addField("Reason", reason)
-	.addField("Warnings:", warnings[user.id].warnings)
-	.addField("Warned By", `${message.author.username}`)
-	.setTimestamp();
-
-/*	let jailEmbed = new Discord.RichEmbed()
-	.setDescription("**Punishment**")
-	.setColor("#8D021F")
-	.addField("Jailed User", `${jUser}`)
-	.addField("Jailed By", `${message.author.username}`)
-	.setTimestamp(); */
+	.setTimestamp()
+	.setDescription(stripIndents`**Warned User:** ${user}
+	**Reason:** ${reason}
+	**Warnings:** ${warnings[user.id].warnings}
+	**Warned By:** ${message.author.username}`);
 
 	message.delete();
 	let warnchannel = message.guild.channels.find(channel => channel.name === "logs");
@@ -51,22 +43,6 @@ try {
 
 	warnchannel.send(warnEmbed);
 
-	if(this.client.warnings.get(`${user.id}`, "warnings") == 1) {
-		user.addrole(wrole.id);
-	}
-
-	message.user.send(`You have been warned for reason.`);
-/*
-	if(this.client.warnings.get(`${user.id}`, "warnings") == 3) {
-		user.removeRoles(user.roles);
-		user.addRole(jrole.id);
-		warnchannel.send(jailEmbed);
-}
-
-setTimeout(function() {
-	jUser.removerole.jrole.id;
-}, ms("24h"));
- */
 }catch (error) {
 	console.log(error);
 }

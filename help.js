4ab4ede8d3e5c60a6233577d1f0ffbd410573ab6
Rayne <56103163+garbagepunks@users.noreply.github.com>
2020-03-01 14:01:10 -0600

    Update help.js

--- help.js
+++ help.js
@@ -1,28 +1,74 @@
 const Discord = require('discord.js');
+const client = require('discord.js');
+const RichEmbed = require('discord.js');
 const { stripIndents } = require("common-tags");
+const { prefix } = require('../config.json');
 
 module.exports = {
 	name: 'help',
 	description: 'Displays a list of commands.',
 	usage: 'help',
-  aliases: 'commands',
+  aliases: ['commands'],
 	execute(message, args) {
-
-  let helpEmbed = new Discord.RichEmbed()
-  .setTitle("Commands")
-  .addField("**Mod Commands:**", stripIndents`Ban
-    Kick
-    Warn
-    Jail
-    Purge`, true)
-  .addField("**All User Commands:**", stripIndents`Help
-    Info
-    Confess
-    Report
-    Suggest
-    8ball`, true)
-  .setColor("#ffff00");
+  
+  const data = [];
+  const { commands } = message.client;
     
-  message.channel.send(helpEmbed);
-},
-};
+    try {
+      
+      if(!args.length) {
+        
+    let helpEmbed = new Discord.RichEmbed()
+    .setTitle(`${message.guild.me.displayName} Help`)
+    .addField("Moderation:", stripIndents`
+    Ban
+    Jail
+    Kick
+    Mute
+    Purge
+    Removewarn
+    Unjail
+    Warn`, true)
+    .addField("Info:", stripIndents`Help
+    ServerInfo
+    Warnings
+    UserInfo`, true)
+    .addBlankField()
+    .addField("Utilities:", stripIndents`Confess
+    Poll
+    QOTD
+    Report
+    Suggest`, true)
+    .addField("Fun:", stripIndents`8Ball
+    Coinflip
+    Kill`, true)
+    .setColor("#ffff00")
+    .setFooter("Use !help {command} for help with a specific command.");
+        
+    message.channel.send(helpEmbed);
+      }
+      
+    else if(args[0])  {
+        
+    const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+       
+    data.push(`**Name:** ${command.name}`);
+        
+      if (!command.name) {
+			return message.reply('That is not a valid command.  Are you using an alias?');
+		}
+        
+   	if (command.description) data.push(`**Description:** ${command.description}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+
+		message.channel.send(data, { split: true });
+      
+}
+    }
+    catch(e) {
+      console.log(e);
+    }
+  }
+  };

8f3462724f83bea3cd061c80e719060135456815
Rayne <56103163+garbagepunks@users.noreply.github.com>
2020-03-01 14:04:44 -0600

    Update and rename warn.js to commands/warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -6,17 +6,20 @@ const warnings = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports = {
 	name: 'warn',
+  category: 'moderation',
 	description: 'Issues a warning to a user.',
-	usage: 'warn [username] [reason]',
-	execute(message, args) {
+	usage: '[username] [reason]',
+	async execute(message, args) {
 
 try {
-	if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You do not have permission to warn someone.");
-	let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You do not have permission to warn someone.");
+	let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 	if(!user) return message.reply("Can't find user.");
 
-	let reason = args.join(" ").slice(22);
+	let wrole = message.guild.roles.get("612716413098131456");
+	let reason = args.slice(1).join(" ");
 	if(!reason) return message.reply("Please provide a reason for warning this user.");
+//	let warnlevel = warnings[user.id].warnings;
 
 	if(!warnings[user.id]) warnings[user.id] = {
 		warnings: 0
@@ -24,7 +27,7 @@ try {
 
 	warnings[user.id].warnings++;
 
-	fs.writeFile("./warnings.json", JSON.stringify(warnings, null, 4), (err) => {
+	await fs.writeFile("./warnings.json", JSON.stringify(warnings, null, 4), (err) => {
 		if (err) console.log(err);
 	});
 
@@ -36,15 +39,20 @@ try {
 	**Reason:** ${reason}
 	**Warnings:** ${warnings[user.id].warnings}
 	**Warned By:** ${message.author.username}`);
-
-	message.delete();
+  
 	let warnchannel = message.guild.channels.find(channel => channel.name === "logs");
 	if(!warnchannel) return message.channel.send("Couldn't find logs channel.");
 
-	warnchannel.send(warnEmbed);
-
+	await warnchannel.send(warnEmbed);
+  
+  await user.addRole(wrole.id);
+  
+  await message.delete(5000);
+  
+await user.send(`You have been warned in ${message.guild.name} for ${reason}.  You have ${warnings[user.id].warnings} warnings.`);
 }catch (error) {
 	console.log(error);
 }
 }
 }
+

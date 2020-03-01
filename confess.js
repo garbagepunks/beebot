bff4e6eaab3b4f119ec2ba22b444832cfb621215
Rayne <56103163+garbagepunks@users.noreply.github.com>
2020-03-01 14:00:47 -0600

    Update confess.js

--- confess.js
+++ confess.js
@@ -1,16 +1,20 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+//const client = new Discord.client();
 
 module.exports = {
 	name: 'confess',
-	description: 'Sends an anonymous confession',
-	usage: 'confess [confession]]',
+	description: 'Send this command as a direct message to Bee Bot and it will post your confession anonymously.',
+	usage: '[confession]',
 	aliases: ['confession'],
-	execute(message, args) {
+	async execute(message, args) {
 
 	if (message.channel.type === "dm") {
 
 	let confession = args.join(" ").slice();
+  
+  if(!args[0]) return message.reply("You can't send an empty confession.");
+
+	let fetchedUser = await message.client.fetchUser("301212599381393409");
 
 	let confEmbed = new Discord.RichEmbed()
 	.setTitle("Anonymous Confession")
@@ -18,14 +22,25 @@ module.exports = {
 	.setColor("#ffff00")
 	.setTimestamp();
 
-	//let confessChannel = message.client.channels.find(channel => channel.name === "confessions");
 	let confessChannel = message.client.channels.get("618350345718071316");
 	if(!confessChannel) return message.reply("Couldn't find confessions channel.");
 
-	confessChannel.send(confEmbed);
-	message.author.send("Confession has been posted.");
-	console.log(`${message.author.username} sent ` + confession);
+	try {
+	await confessChannel.send(confEmbed);
+	await message.author.send("Confession has been posted.");
+	await fetchedUser.send(`${message.author.username} sent ` + confession);
+} catch(e) {
+	console.log(e);
+}
 
 	}
+    if(!message.guild === "606898838254518405") {
+      try {
+        message.channel.send("I can only post confessions that were sent to me in a DM.");
+      }catch (e){
+        console.log(e)
+      }
+    }
 }
 };
+

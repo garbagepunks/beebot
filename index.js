b8d3d310c45c54d4e17cd489784ead2de39dccb8
Rayne <56103163+garbagepunks@users.noreply.github.com>
2020-03-01 14:00:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+const { stripIndents } = require("common-tags");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+this.channels = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -18,7 +32,9 @@ client.once('ready', () => {
 
 client.on('message', message => {
 
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if(message.author.bot) return;
+
+  if (message.content.startsWith(prefix)) {
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
@@ -27,55 +43,71 @@ client.on('message', message => {
 		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
  	if (!command) return;
-
+  
 try {
 	command.execute(message, args);
 } catch (error) {
 	console.error(error);
 	message.reply('There was an error trying to execute that command.');
-}
+} 
+  
+ } else if(!message.content.startsWith(prefix)) { 
+    
+  //Dating Channels React  
+
+ if(message.channel.id === "619965922903326742" || message.channel.id === "619965965068795904") {
+    try {
+    message.react('❤️')
+                  .then(() => message.react('💔'));
+  }catch(e) {
+    console.log(e);
+  };
+ }; //Close dating channels react
+ }; //Close else if
+ }); // Close message listener
+
+
+//Cache New Members
+client.on('guildMemberAdd', (member) => {
+  client.fetchUser(member.id, true);
 });
+
 
 //Send Goodbye Message
 client.on('guildMemberRemove', (member) => {
+  
+  var goodbyes = [
+	`Press F to pay respects to **${member.displayName}**.`,
+	`**${member.displayName}** has ascended.`,
+	`The wild **${member.displayName}** used teleport.`,
+	`Why did you set me on fire, **${member.displayName}**? Why didn\'t you just write your essay?`,
+	`**${member.displayName}**: Gone but not forgotten.`,
+	`**${member.displayName}** has decided to leave the server...Effective immediately.`,
+	`Ouch, **${member.displayName}** let the door hit them on the way out.`,
+	`Farewell, **${member.displayName}**.`,
+	`**${member.displayName}** lost the game.`,
+  `**${member.displayName}** has abandoned us in this hell hole.`,
+	`**${member.displayName}** has bailed on us.`,
+  `**${member.displayName}** vanished in a puff of smoke.`,
+  `**${member.displayName}** disappeared without a trace.`,
+  `Adieu, Adieu, to you and you and **${member.displayName}**.`,
+  `Way to go, guys. You bullied **${member.displayName}** out of the server.`,
+	`In the words of Fall Out Boy, thanks for the memories, **${member.displayName}**.`,
+	`**${member.displayName}** went MIA.`,
+  `**${member.displayName}** couldn\'t hang.`,
+	`**${member.displayName}** has abandoned me...just like my father.`,
+	`Ding, Dong! The **${member.displayName}** is dead!`];
 
-  var facts = [
-	`Press F to pay respects to **${member.user}**.`,
-	`**${member.user}** has ascended.`,
-	`The wild **${member.user}** used teleport.`,
-	`Why did you set me on fire, **${member.user}**? Why didn\'t you just write your essay?`,
-	`**${member.user}**: Gone but not forgotten.`,
-	`**${member.user}** has decided to leave the server...Effective immediately.`,
-	`Ouch, **${member.user}** let the door hit them on the way out.`,
-	`Farewell, **${member.user}**.`,
-	`**${member.user}** lost the game.`,
-	`**${member.user}** has bailed on us.`,
-	`Thanks for the memories, **${member.user}**.`,
-	`**${member.user}** went MIA.`,
-	`**${member.user}** has abandoned me...just like my father.`,
-	`Ding, Dong! The **${member.user}** is dead!`];
+	var goodbyemessage = Math.floor(Math.random() * goodbyes.length);
 
-	var fact = Math.floor(Math.random() * facts.length);
-
-//	let leaveChannel = client.channels.find(channel => channel.name === "entrances and exits");
 
 try {
-//	leaveChannel.send(facts[fact]);
-		client.channels.get("606901598635163708").send(facts[fact]);
+		client.channels.get("606901598635163708").send(goodbyes[goodbyemessage]);
 } catch(error){
     console.log("[ERROR]",error)}
-});
-
-//Send Ban Message
-client.on('guildBanAdd', member => {
-
-let banChannel = client.channels.find(channel => channel.name === "entrances and exits");
-
-try {
-		banChannel.send(`**${member.user}** has felt the wrath of the ban hammer.`);
-} catch(error){
-    console.log("[ERROR]",error)}
-});
+}
+);
 
 
-client.login(token);
+
+client.login(process.env.TOKEN);

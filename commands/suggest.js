224e617affe49dfef97a758e641c463c654e6aac
Rayne <56103163+garbagepunks@users.noreply.github.com>
2020-03-01 14:03:40 -0600

    Update and rename suggest.js to commands/suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,23 +3,28 @@ const client = new Discord.Client();
 
 module.exports = {
 	name: 'suggest',
-	description: 'Logs server suggestions.',
-	usage: 'suggest [suggestion]',
+  category: 'moderation',
+	description: 'Logs server suggestions to a suggestions channel and allows reactions.',
+	usage: '[suggestion]',
 	aliases: ['suggestion'],
 	execute(message, args) {
 
-	let suggestion = args.join(" ").slice();
+	let suggestion = args.join(" ");
+  if(!suggestion) return;
 
 	let sugEmbed = new Discord.RichEmbed()
 	.setTitle("Server Suggestion")
 	.setDescription(suggestion)
 	.setColor("#ffff00")
+  .setFooter(`Suggested by ${message.author.username}`)
 	.setTimestamp();
 
 	let sugChannel = message.guild.channels.find(channel => channel.name === "suggestions");
-	if(!sugChannel) return message.channel.send("Can't find channel.");
+	if(!sugChannel) return message.author.send("Can't find channel.");
+    
+  if(message.guild === null) return message.channel.send("Please send this in a server channel, not a DM!");
 
-	message.delete();
+	message.delete(5000);
 	sugChannel.send(sugEmbed).then(async embedMessage => {
 		await embedMessage.react('✅');
 		await embedMessage.react('❌');
@@ -29,3 +34,4 @@ module.exports = {
 
 	},
 };
+
